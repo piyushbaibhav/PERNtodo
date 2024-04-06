c0dff2dcc62342e7ce8c5688e2c48987bb855a16
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-04-07 02:41:12 +0530

    comments

--- server/db.js
+++ server/db.js
@@ -8,3 +8,16 @@ const pool = new Pool({
 });
 
 export default pool;
+
+
+// const Pool = require("pg").Pool;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "kthl8822",
+//   host: "localhost",
+//   port: 5432,
+//   database: "perntodo",
+// });
+
+// module.exports = pool;
